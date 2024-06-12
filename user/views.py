c16d52b0d7b06b45db771e0c12c1a2cf59da9a3d
Olexander Bryl <edlrian814@gmail.com>
2024-06-12 18:26:12 +0300

    Add CreateEmployee view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from user.serializers import UserCreateSerializer
+
+
+class CreateEmployee(CreateAPIView):
+    serializer_class = UserCreateSerializer
+    queryset = get_user_model().objects.all()
+    permission_classes = [IsAdminUser]
+
+    def perform_create(self, serializer):
+        return serializer.save(is_employee=True)
